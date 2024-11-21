e387bf36f2595ad7a473955f71402d36708a4887
Damir <mainboom6@gmail.com>
2024-11-21 19:07:52 +0300

    A calculator has been created

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 'use strict'
 
+
 window.addEventListener(('DOMContentLoaded'), () => {
     //Tabs
-
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabParent = document.querySelector('.tabheader__items'),
         tabContent = document.querySelectorAll('.tabcontent');
@@ -392,6 +392,79 @@ window.addEventListener(('DOMContentLoaded'), () => {
             changeDot()
         })
     })
+
+    // Калькулятор уточной нормы калорий
+
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+        sex = 'woman';
+        ratio = 1.375;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if(sex == 'woman') {
+            result.textContent = Math.round(((height * 1.8) - (age * 4.7) + (weight * 9.6) + 655) * ratio)
+        } else {
+            result.textContent = Math.round(((height * 5) - (age * 6.8) + (weight * 13.7) + 66) * ratio )           
+        }
+    }
+
+    calcTotal()
+
+    function getStaticInformation(parentSelector, classActiv) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio')
+                } else {
+                    sex = e.target.getAttribute('id')
+                }
+
+                console.log(ratio, sex);
+
+                elements.forEach(element => {
+                    element.classList.remove(classActiv)
+                })
+
+                e.target.classList.add(classActiv)
+                calcTotal()
+
+            })
+        })
+
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDInamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+             calcTotal()
+        })
+    }
+
+    getDInamicInformation('#height')
+    getDInamicInformation('#weight')
+    getDInamicInformation('#age')
 })
 
 
